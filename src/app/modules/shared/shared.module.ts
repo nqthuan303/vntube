cb0380f81c2d168c5f740d1713b50749b7c0a241
Thuan <nqthuan303@gmail.com>
2017-06-04 21:39:19 +0700

    added upload component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule }       from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ConfirmModalComponent } from './components/confirmModal.component';
 import { UploadModalComponent } from './components/uploadModal.component';
 import { ModalModule } from 'ngx-bootstrap';
 import { UploadComponent } from './components/upload.component';
+import { FileUploadModule, FileSelectDirective, FileDropDirective } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,7 @@ import { UploadComponent } from './components/upload.component';
     UploadModalComponent,
     UploadComponent
   ],
-  imports: [ModalModule.forRoot()],
+  imports: [FileUploadModule, CommonModule, ModalModule.forRoot()],
   exports:[ ConfirmModalComponent, UploadModalComponent ]
 })
 export class SharedModule {}
